2117b9be62b3193b183765894680cc69dbca3f3f
Yatharth4005 <kaushiksp04@gmail.com>
2026-05-26 21:52:02 +0530

    fix: CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ const app = express();
 const httpServer = createServer(app);
 
 app.use(cors());
+app.options("*", cors()); // Enable preflight handling for all routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
